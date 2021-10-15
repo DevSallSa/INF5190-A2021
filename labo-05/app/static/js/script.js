7d12376d991adfa8e4f8e73d62912af4da6cefe1
Maxime Masson <maxime.masson@nurun.com>
2021-10-14 23:49:33 -0400

    add TODO ajax

--- labo-05/app/static/js/script.js
+++ labo-05/app/static/js/script.js
@@ -1,1 +1,14 @@
-console.log("toto")
+console.log("Mounted !");
+
+// Appel AJAX avec fetch()
+const getBookById = (id) => {
+    /**
+     * TODO: Utiliser fetch() pour une appelle AJAX au backend
+     * et retourner les informations d'un livre en particulier
+     * ref: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+     * youtube: https://www.youtube.com/watch?v=RvYYCGs45L4&ab_channel=Fireship
+     * youtube 2 : https://www.youtube.com/watch?v=ITogH7lJTyE&ab_channel=Fireship
+     */
+
+    return;
+};
